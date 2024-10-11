cc212cc177b23bdd0bc24927d68ef0f96e8eba15
extrymes <65893656+extrymes@users.noreply.github.com>
2024-10-11 23:06:13 +0200

    feat(engine): add raycasting and rendering

---
+++ engine/raycasting.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 22:23:22 by sabras            #+#    #+#             */
+/*   Updated: 2024/10/11 15:45:50 by sabras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static t_ray	*cast_ray(t_data *data, double plane_x, double plane_y, int x);
+static void		hit_wall(t_data *data, t_ray *ray);
+static int		get_wall_face(t_ray *ray);
+
+void	raycasting(t_data *data)
+{
+	t_ray	*ray;
+	double	fov_plane_scale;
+	int		x;
+
+	render_background(data);
+	fov_plane_scale = tanf((FOV * (M_PI / 180.0)) / 2.0);
+	data->player->plane_x = -data->player->dir_y * fov_plane_scale;
+	data->player->plane_y = data->player->dir_x * fov_plane_scale;
+	x = 0;
+	while (x < data->win_width)
+	{
+		ray = cast_ray(data, data->player->plane_x, data->player->plane_y, x);
+		render_column(data, ray, x);
+		x += VERTICAL_SLICING;
+		free(ray);
+	}
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->img_tab[RENDERING].ptr, 0, 0);
+}
+
+static t_ray	*cast_ray(t_data *data, double plane_x, double plane_y, int x)
+{
+	t_ray	*ray;
+
+	ray = init_ray(data, plane_x, plane_y, x);
+	hit_wall(data, ray);
+	ray->wall_face = get_wall_face(ray);
+	if (ray->wall_side == 0)
+		ray->per_wall_dist = (ray->rough_x - data->player->pos_x + (1 - ray->step_x) / 2) / ray->dir_x;
+	else
+		ray->per_wall_dist = (ray->rough_y - data->player->pos_y + (1 - ray->step_y) / 2) / ray->dir_y;
+	return (ray);
+}
+
+static void	hit_wall(t_data *data, t_ray *ray)
+{
+	while (1)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->rough_x += ray->step_x;
+			ray->wall_side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->rough_y += ray->step_y;
+			ray->wall_side = 1;
+		}
+		if (ray->rough_y < 0 || ray->rough_x < 0
+		 || ray->rough_y >= len_tab(data->map->map) || ray->rough_x >= (int)ft_strlen(data->map->map[ray->rough_y]))
+			return (free(ray), throw_error(data, "ray is out of map"));
+		if (data->map->map[ray->rough_y][ray->rough_x] == '1')
+			break ;
+	}
+}
+
+static int	get_wall_face(t_ray *ray)
+{
+	if (ray->wall_side == 0)
+	{
+		if (ray->dir_x > 0)
+			return (WALL_WE);
+		return (WALL_EA);
+	}
+	else
+	{
+		if (ray->dir_y > 0)
+			return (WALL_NO);
+		return (WALL_SO);
+	}
+}
---
+++ engine/rendering.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 00:03:56 by sabras            #+#    #+#             */
+/*   Updated: 2024/10/11 13:40:05 by sabras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static int	get_color(t_data *data, t_ray *ray, int col_height, int y);
+static int	get_column(t_ray *ray, t_img *texture);
+
+void	render_column(t_data *data, t_ray *ray, int x)
+{
+	int	col;
+	int	col_height;
+	int	col_start;
+	int	col_end;
+	int	y;
+
+	col_height = (int)(data->win_height / ray->per_wall_dist);
+	col_start = -col_height / 2 + data->win_height / 2;
+	if (col_start < 0)
+		col_start = 0;
+	col_end = col_height / 2 + data->win_height / 2;
+	if (col_end >= data->win_height)
+		col_end = data->win_height - 1;
+	col = x;
+	while (x < col + VERTICAL_SLICING)
+	{
+		y = col_start;
+		while (y < col_end)
+		{
+			put_pixel_on_img(&data->img_tab[RENDERING], x, y,
+				get_color(data, ray, col_height, y));
+			y++;
+		}
+		x++;
+	}
+}
+
+void	render_background(t_data *data)
+{
+	ft_memcpy(data->img_tab[RENDERING].addr, data->img_tab[BACKGROUND].addr,
+		data->background_size);
+}
+
+static int	get_color(t_data *data, t_ray *ray, int col_height, int y)
+{
+	t_img	texture;
+	int		tex_x;
+	int		tex_y;
+	int		color;
+	char	*pixel;
+
+	texture = data->img_tab[ray->wall_face];
+	tex_x = get_column(ray, &texture);
+	tex_y = (int)(((y - (-col_height / 2 + data->win_height / 2))
+		* texture.height) / col_height);
+	if (tex_y < 0)
+		tex_y = 0;
+	if (tex_y >= texture.height)
+		tex_y = texture.height - 1;
+	pixel = texture.addr + (tex_y * texture.line_length + \
+	tex_x * (texture.bits_per_pixel / 8));
+	color = *(unsigned int *)pixel;
+	return (color);
+}
+
+static int	get_column(t_ray *ray, t_img *texture)
+{
+	int		tex_x;
+	double	wall_hit;
+
+	if (ray->wall_side == 0)
+		wall_hit = ray->pos_y + ray->per_wall_dist * ray->dir_y;
+	else
+		wall_hit = ray->pos_x + ray->per_wall_dist * ray->dir_x;
+	wall_hit -= floor(wall_hit);
+	tex_x = (int)(wall_hit * (double)(texture->width));
+	if ((ray->wall_side == 0 && ray->dir_x > 0)
+		|| (ray->wall_side == 1 && ray->dir_y < 0))
+		tex_x = texture->width - tex_x - 1;
+	return (tex_x);
+}
