0057769a0170ee9fc17fad6ee9bdd7ee086ef87c
extrymes <65893656+extrymes@users.noreply.github.com>
2024-10-20 17:39:59 +0200

    chore(utils): return time in ms with `get_current_time()` function

--- src/utils/time.c
+++ src/utils/time.c
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:49:32 by sabras            #+#    #+#             */
-/*   Updated: 2024/10/14 17:53:23 by sabras           ###   ########.fr       */
+/*   Updated: 2024/10/18 16:20:09 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ double	get_current_time(void)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return (tv.tv_sec + tv.tv_usec / 1e6);
+	return ((tv.tv_sec * 1000.0) + (tv.tv_usec / 1000.0));
 }
