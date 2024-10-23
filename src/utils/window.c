3af69762f82e002260f5bb459de5aaaff32188f4
extrymes <65893656+extrymes@users.noreply.github.com>
2024-10-23 20:05:06 +0200

    feat: use a new `create_window()` function

--- src/utils/window.c
+++ src/utils/window.c
@@ -1,31 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   window.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/18 09:09:13 by sabras            #+#    #+#             */
-/*   Updated: 2024/10/23 20:03:22 by sabras           ###   ########.fr       */
+/*   Created: 2024/10/23 14:23:14 by sabras            #+#    #+#             */
+/*   Updated: 2024/10/23 14:43:23 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(int ac, char **av)
+void	create_window(t_data *data)
 {
-	t_data	data;
-
-	if (ac != 2)
-		return (ft_putstr_fd(RED "Usage:\n\t./cub3d <map.cub>\n" RESET, 2), 1);
-	data = init_data();
-	check_map(av[1], &data);
-	create_window(&data);
-	create_images(&data);
-	draw_background(&data);
-	render_scene(&data, get_current_time());
-	SET_FONT(data.mlx_ptr, data.win_ptr, "10x20");
-	track_mouse(&data);
-	handle_events(&data);
-	return (clear_data(&data), 0);
+	data->mlx_ptr = mlx_init();
+	if (!data->mlx_ptr)
+		throw_error(data, "Cannot init mlx");
+	GET_SCREEN_SIZE(data->mlx_ptr, &data->win_width, &data->win_height);
+	data->win_ptr = mlx_new_window(data->mlx_ptr, data->win_width,
+			data->win_height, WIN_TITLE);
+	if (!data->win_ptr)
+		throw_error(data, "Cannot create window");
 }
